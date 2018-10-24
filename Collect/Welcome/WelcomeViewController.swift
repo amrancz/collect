c20908bf2dd1d88e4542803eeabf1d7b3acce99b
Adam <Adam@adhams-macbook-pro.local>
2018-10-24 07:57:45 +0200

    Import screenshots function on Welcome screen

--- Collect/Welcome/WelcomeViewController.swift
+++ Collect/Welcome/WelcomeViewController.swift
@@ -10,8 +10,45 @@ import Foundation
 import UIKit
 import Realm
 import RealmSwift
+import Photos
 import BSImagePicker
 
 class WelcomeViewController: UIViewController {
     
+    var screenshotImage: UIImage!
+    var screenshotUUID: String?
+    var screenshotPosition: Int?
+    var screenshotImageSet: [UIImage?] = []
+    
+    //MARK: UIImagePicker to add screenshots
+    @IBAction func addScreenshotButton(_ sender: Any) {
+        let screenshotPicker = BSImagePickerViewController()
+        bs_presentImagePickerController(screenshotPicker, animated: true,
+                                        select: { (asset: PHAsset) -> Void in
+        }, deselect: { (asset: PHAsset) -> Void in
+        }, cancel: { (assets: [PHAsset]) -> Void in
+        }, finish: { (assets: [PHAsset]) -> Void in
+            for asset in assets {
+                let manager = PHImageManager.default()
+                let option = PHImageRequestOptions()
+                var image = UIImage()
+                option.isSynchronous = true
+                manager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+                    image = result!
+                })
+                let imageData = image.pngData() as Data?
+                let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                let uuid = UUID().uuidString
+                let screenshot = Screenshot()
+                let writePath = documentsDirectory.appendingPathComponent("\(uuid).png")
+                try! imageData?.write(to: writePath as URL, options: [.atomic])
+                screenshot.screenshotID = uuid
+                screenshot.screenshotFileName = "\(uuid).png"
+                let realm = try! Realm()
+                try! realm.write {
+                    realm.add(screenshot)
+                }
+            }
+        }, completion: nil)
+    }
 }
